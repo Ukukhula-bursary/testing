4c17f878486100f535e5045ac30677af14c592db
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-16 12:55:47 +0200

    add script for student aplication table creation

---
+++ UkukhulaBursary/Tables/StudentApplication.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [dbo].[StudentApplication]
+(
+  [ID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  [StudentID] INT NOT NULL,
+  [Motivation] TEXT NOT NULL,
+  [RejectionReason] TEXT NOT NULL DEFAULT 'N/A',
+  [BursaryAmount] MONEY NOT NULL,
+  [Status] INT NOT NULL DEFAULT "Review",
+  [Date] DATE NOT NULL DEFAULT GETDATE()
+)
+GO
+
+
+ALTER TABLE [dbo].[StudentApplication]
+  ADD CONSTRAINT [FK_StudentApplicationStudentID] FOREIGN KEY([StudentID]) REFERENCES [dbo].[Student]
+GO
