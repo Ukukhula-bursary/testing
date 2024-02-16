33675cb397f8c68b636542ea667d876646b66fd4
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-16 13:10:35 +0200

    add script for creating student allocation table

--- UkukhulaBursary/Tables/StudentAllocation.sql
+++ UkukhulaBursary/Tables/StudentAllocation.sql
@@ -3,12 +3,12 @@ CREATE TABLE [dbo].[StudentAllocation]
   [ID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
   [StudentID] INT NOT NULL,
   [Amount] MONEY NOT NULL,
-  [Year] INT NOT NULL
+  [Year] INT NOT NULL DEFAULT YEAR(GETDATE()),
 );
 GO
 
-ALTER TABLE [dbo].[YearlyStudentAllocation]
-  ADD CONSTRAINT [FK_YearlyStudentAllocation_Student_StudentID]
+ALTER TABLE [dbo].[StudentAllocation]
+  ADD CONSTRAINT [FK_StudentAllocationStudentID]
       FOREIGN KEY ([StudentID])
       REFERENCES [dbo].[Student]([StudentID])
 GO
