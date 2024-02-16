d4f9002a35cd7bd6ee1ffefc61671258f6cbf3ca
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-16 11:47:16 +0200

    add script for creating user table

---
+++ UkukhulaBursary/Tables/User.sql
@@ -0,0 +1,18 @@
+CREATE TABLE [dbo].[User]
+(
+  [ID] INT IDENTITY(1,1) PRIMARY KEY NOT NULL ,
+  [FirstName] VARCHAR(50) NOT NULL,
+  [LastName] VARCHAR(120) NOT NULL,
+  [ContactID] INT NOT NULL,
+  [UserRoleID] INT NOT NULL,
+  [IsUserActive] BIT NOT NULL DEFAULT 1
+)
+GO
+
+ALTER TABLE [dbo].[User]
+ADD CONSTRAINT FK_UserRole FOREIGN KEY([UserRoleID]) REFERENCES [dbo].[UserRole]
+GO
+
+ALTER TABLE [dbo].[User]
+ADD CONSTRAINT FK_Contact FOREIGN KEY([ContactID]) REFERENCES [dbo].[Contact]
+GO
