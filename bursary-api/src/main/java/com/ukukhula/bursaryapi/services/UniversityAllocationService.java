e532a6d2c6ea4cb8afa6530d3df178fc50c88d17
tebogomotibane <tebogomotibane@gmail.com>
2024-02-17 13:14:03 +0200

    added university allocation service

---
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/services/UniversityAllocationService.java
@@ -0,0 +1,21 @@
+package com.ukukhula.bursaryapi.services;
+
+import com.ukukhula.bursaryapi.entities.UniversityAllocation;
+import com.ukukhula.bursaryapi.repositories.UniversityAllocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UniversityAllocationService {
+    @Autowired
+    private final UniversityAllocationRepository universityAllocationRepository;
+
+    public UniversityAllocationService(UniversityAllocationRepository universityAllocationRepository) {
+        this.universityAllocationRepository = universityAllocationRepository;
+    }
+
+      //  call to the repository to fetch UniversityAllocation by ID
+    public UniversityAllocation findUniversityAllocationById(int id) {
+        return universityAllocationRepository.findById(id);
+    }
+}
