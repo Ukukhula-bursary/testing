80e558e7bf45c7ebb1215668afb04e7136ca2e5c
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-17 16:17:11 +0200

    add end-point to add a university

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/UniversityController.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/UniversityController.java
@@ -6,6 +6,9 @@ import com.ukukhula.bursaryapi.entities.University;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import com.ukukhula.bursaryapi.services.UniversityService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +20,16 @@ public class UniversityController {
   @Autowired
   private UniversityService universityService;
 
+  @PostMapping("/universities")
+  University newUniversity(@RequestBody University newUniversity) {
+    return universityService.addUniversity(newUniversity.getName());
+  }
+
   @GetMapping("/universities/{id}")
-  EntityModel<University> oneUniversity(@PathVariable int id) {
+  EntityModel<University> one(@PathVariable int id) {
 
     University university = universityService.getUniversityById(id);
     return EntityModel.of(university,
-        linkTo(methodOn(UniversityController.class).oneUniversity(id)).withSelfRel());
+        linkTo(methodOn(UniversityController.class).one(id)).withSelfRel());
   }
 }
