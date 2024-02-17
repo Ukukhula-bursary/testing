97d89b17e020bc5afe93265f613a33c63b396cd1
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-17 22:55:24 +0200

    add and import assembler

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/StudentApplicationController.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/StudentApplicationController.java
@@ -1,11 +1,7 @@
 package com.ukukhula.bursaryapi.controllers;
 
-import javax.swing.text.html.parser.Entity;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ukukhula.bursaryapi.assemblers.StudentApplicationAssembler;
 import com.ukukhula.bursaryapi.entities.StudentApplication;
 import com.ukukhula.bursaryapi.services.StudentApplicationService;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class StudentApplicationController {
