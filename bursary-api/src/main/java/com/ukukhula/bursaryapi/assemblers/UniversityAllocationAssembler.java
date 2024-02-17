0e3db75a72cf35fc7beeab7e174cecf1ae4412c5
tebogomotibane <tebogomotibane@gmail.com>
2024-02-18 00:22:06 +0200

    added assembler

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/assemblers/UniversityAllocationAssembler.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/assemblers/UniversityAllocationAssembler.java
@@ -1,5 +1,22 @@
 package com.ukukhula.bursaryapi.assemblers;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
-public class UniversityAllocationAssembler {
-    
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import com.ukukhula.bursaryapi.controllers.UniversityAllocationController;
+import com.ukukhula.bursaryapi.entities.UniversityAllocation;
+
+@Component
+public class UniversityAllocationAssembler implements RepresentationModelAssembler<UniversityAllocation, EntityModel<UniversityAllocation>> {
+
+  @Override
+  public EntityModel<UniversityAllocation> toModel(UniversityAllocation universityAllocation) {
+    return EntityModel.of(universityAllocation,
+        linkTo(methodOn(UniversityAllocationController.class).getUniversityAllocationById(universityAllocation.getId())).withSelfRel()
+        // linkTo(methodOn(UniversityAllocationController.class).getAllUniversityAllocations()).withRel("allocations")
+        );
+  }
+
 }
