31047ff446dd0675b4c5f01348e1e0575b94e35e
tebogomotibane <tebogomotibane@gmail.com>
2024-02-18 00:22:32 +0200

    modified controller to accomodate assembler

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/UniversityAllocationRepository.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/UniversityAllocationRepository.java
@@ -1,7 +1,10 @@
 package com.ukukhula.bursaryapi.repositories;
 
+import java.util.List;
+
 import com.ukukhula.bursaryapi.entities.UniversityAllocation;
 
 public interface UniversityAllocationRepository {
     UniversityAllocation findById(int id);
+   List<UniversityAllocation> getAllStudentAllocations();
 }
