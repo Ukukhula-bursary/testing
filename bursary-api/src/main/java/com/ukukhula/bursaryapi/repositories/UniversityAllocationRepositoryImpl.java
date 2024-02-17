50f30cb6cebe0e64723b81c918a567682d21e366
tebogomotibane <tebogomotibane@gmail.com>
2024-02-17 13:13:27 +0200

    added university allocation repository implementation

---
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/UniversityAllocationRepositoryImpl.java
@@ -0,0 +1,41 @@
+
+package com.ukukhula.bursaryapi.repositories;
+
+import com.ukukhula.bursaryapi.entities.University;
+import com.ukukhula.bursaryapi.entities.UniversityAllocation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UniversityAllocationRepositoryImpl implements UniversityAllocationRepository {
+    final String SQL = "SELECT * FROM UniversityAllocation WHERE ID = ?"; // procedure
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    // public UniversityAllocation findById(int id) {
+
+    // // Query Execution: jdbcTemplate.queryForObject() executes the SQL query.
+    // // parameters should go in order that the query expects them , like when
+    // using $
+    // // return jdbcTemplate.queryForObject(SQL,
+    // // new
+    // BeanPropertyRowMapper<UniversityAllocation>(UniversityAllocation.class), id);
+
+    // }
+
+    public UniversityAllocation findById(int id) {
+        return jdbcTemplate.queryForObject(SQL, UniversityAllocationRowMapper,
+                id);
+    }
+
+    private final RowMapper<UniversityAllocation> UniversityAllocationRowMapper = ((resultSet,
+            rowNumber) -> {
+        return new UniversityAllocation(resultSet.getInt("ID"), resultSet.getInt("UniversityID"),
+                resultSet.getBigDecimal("Amount"), resultSet.getInt("BursaryDetailsID"));
+    });
+
+    // ADD SERVICES - class or interface and implementation??
+
+}
