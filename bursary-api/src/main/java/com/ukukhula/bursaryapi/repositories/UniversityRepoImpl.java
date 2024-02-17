f4529e589e2c6dcc559e787ebe160457a6d4dcfb
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-17 21:02:34 +0200

    add repository for getting all the universities

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/UniversityRepoImpl.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/UniversityRepoImpl.java
@@ -2,6 +2,7 @@ package com.ukukhula.bursaryapi.repositories;
 
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -18,6 +19,7 @@ public class UniversityRepoImpl implements UniversityRepository {
 
   private static final String INSERT_UNIVERSITY = "INSERT INTO University (Name) VALUES (?)";
   private static final String GET_INIVERSITY_BY_ID = "EXEC [dbo].[uspGetUniversityById] ?";
+  private static final String GET_ALL_UNIVERSITIES = "SELECT * FROM University";
 
   @Autowired
   JdbcTemplate jdbcTemplate;
@@ -46,6 +48,11 @@ public class UniversityRepoImpl implements UniversityRepository {
   public University getUniversityById(int id) {
     return jdbcTemplate.queryForObject(GET_INIVERSITY_BY_ID, universityRowMapper,
         id);
+  }
+
+  @Override
+  public List<University> getAllUniversities() {
+    return jdbcTemplate.query(GET_ALL_UNIVERSITIES, universityRowMapper);
   }
 
   private final RowMapper<University> universityRowMapper = ((resultSet,
