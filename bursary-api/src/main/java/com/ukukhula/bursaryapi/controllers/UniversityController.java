8276b5c364b93ceb13228c684a1f14e3d48117e0
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-17 16:37:30 +0200

    move model assembler to a separate file

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/UniversityController.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/UniversityController.java
@@ -2,23 +2,27 @@ package com.ukukhula.bursaryapi.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ukukhula.bursaryapi.assemblers.UniversityModelAssembler;
 import com.ukukhula.bursaryapi.entities.University;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.ukukhula.bursaryapi.services.UniversityService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 public class UniversityController {
 
-  @Autowired
   private UniversityService universityService;
+
+  private final UniversityModelAssembler assembler;
+
+  UniversityController(UniversityModelAssembler assembler, UniversityService universityService) {
+    this.assembler = assembler;
+    this.universityService = universityService;
+  }
 
   @PostMapping("/universities")
   University newUniversity(@RequestBody University newUniversity) {
@@ -26,10 +30,9 @@ public class UniversityController {
   }
 
   @GetMapping("/universities/{id}")
-  EntityModel<University> one(@PathVariable int id) {
+  public EntityModel<University> one(@PathVariable int id) {
 
     University university = universityService.getUniversityById(id);
-    return EntityModel.of(university,
-        linkTo(methodOn(UniversityController.class).one(id)).withSelfRel());
+    return assembler.toModel(university);
   }
 }
