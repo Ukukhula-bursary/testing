c7b4dc343bc430fa613b78aa83eb05c8445fcdb7
tebogomotibane <tebogomotibane@gmail.com>
2024-02-18 10:44:53 +0200

    added tp service method for updating one university funds

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/services/UniversityAllocationService.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/services/UniversityAllocationService.java
@@ -4,6 +4,7 @@ import com.ukukhula.bursaryapi.entities.University;
 import com.ukukhula.bursaryapi.entities.UniversityAllocation;
 import com.ukukhula.bursaryapi.repositories.UniversityAllocationRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,11 @@ public class UniversityAllocationService {
     public List<UniversityAllocation> getAllAllocations() {
         return universityAllocationRepository.getAllStudentAllocations();
       }
+
+       public Integer allocateFundsToUniversity(int id, BigDecimal amount) {
+        // Call the repository method to update the allocation amount for the university
+        return universityAllocationRepository.allocateFundsToUniversity(id, amount);
+    }
 
     
 }
