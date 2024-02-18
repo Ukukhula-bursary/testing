d45daaab1fcd54594cfa9c0adeb1f7092e9f3008
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-18 17:48:12 +0200

    add putmapping and update route status

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/StudentApplicationController.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/StudentApplicationController.java
@@ -1,15 +1,19 @@
 package com.ukukhula.bursaryapi.controllers;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ukukhula.bursaryapi.ApplicationStatus;
 import com.ukukhula.bursaryapi.assemblers.StudentApplicationAssembler;
 import com.ukukhula.bursaryapi.entities.StudentApplication;
 import com.ukukhula.bursaryapi.services.StudentApplicationService;
@@ -25,7 +29,7 @@ public class StudentApplicationController {
         this.assembler = assembler;
     }
 
-    @GetMapping("/{studentId}")
+    @GetMapping("/student/{studentId}")
     public EntityModel<StudentApplication> getStudentApplications(@PathVariable int studentId) {
         StudentApplication application = studentApplicationService.findByStudentID(studentId);
         return assembler.toModel(application);
@@ -41,4 +45,23 @@ public class StudentApplicationController {
         return CollectionModel.of(applications);
     }
 
+    @PutMapping("/status/{studentID}")
+    public ResponseEntity<?> updateStudentsApplicationStatus(@PathVariable int studentID,
+            @RequestBody Map<String, String> requestBody) {
+        String statusString = requestBody.get("status");
+
+        try {
+ 
+            Integer rowsAffected = studentApplicationService.updateStudentsApplicationStatus(studentID, statusString);
+
+            if (rowsAffected >= 1) {
+                return ResponseEntity.ok("Student status successful");
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Invalid status value");
+        }
+
+    }
 }
