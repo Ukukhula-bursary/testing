1b51832a1502ae11204fab833eba9f745a92e295
tebogomotibane <tebogomotibane@gmail.com>
2024-02-18 10:43:29 +0200

    added abstract method for updating funds one university

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/UniversityAllocationRepository.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/UniversityAllocationRepository.java
@@ -1,10 +1,14 @@
 package com.ukukhula.bursaryapi.repositories;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import com.ukukhula.bursaryapi.entities.UniversityAllocation;
 
 public interface UniversityAllocationRepository {
     UniversityAllocation findById(int id);
-   List<UniversityAllocation> getAllStudentAllocations();
+
+    List<UniversityAllocation> getAllStudentAllocations();
+
+    Integer allocateFundsToUniversity(int id, BigDecimal amount);
 }
