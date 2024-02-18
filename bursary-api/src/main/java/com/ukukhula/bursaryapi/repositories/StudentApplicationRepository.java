9c70902e5b309ad694101051324c1a02b4389df0
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-18 16:03:52 +0200

    add updateStudentsApplicationStatus

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/StudentApplicationRepository.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/StudentApplicationRepository.java
@@ -1,5 +1,6 @@
 package com.ukukhula.bursaryapi.repositories;
 
+import com.ukukhula.bursaryapi.ApplicationStatus;
 import com.ukukhula.bursaryapi.entities.StudentApplication;
 
 import java.util.List;
@@ -8,4 +9,6 @@ public interface StudentApplicationRepository {
     StudentApplication findByStudentID(int studentID);
 
     List<StudentApplication> getAllStudentsApplications();
+
+    Integer updateStudentsApplicationStatus(int studentID, String status);
 }
