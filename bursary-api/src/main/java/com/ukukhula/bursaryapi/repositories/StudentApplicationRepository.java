f8cccf2f51d27f478d43237672525ef424d2fd8d
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-18 10:24:55 +0200

    add getAllStudentsApplications method.

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/StudentApplicationRepository.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/StudentApplicationRepository.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface StudentApplicationRepository {
     StudentApplication findByStudentID(int studentID);
+
+    List<StudentApplication> getAllStudentsApplications();
 }
