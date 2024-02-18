61395de7a9444530a7694fe023e1b3c8ccb6c0d7
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-18 10:31:21 +0200

    clean up

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/StudentApplicationController.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/StudentApplicationController.java
@@ -1,7 +1,6 @@
 package com.ukukhula.bursaryapi.controllers;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.hateoas.CollectionModel;
