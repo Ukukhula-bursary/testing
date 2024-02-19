66350134c4dbfb6b4a6b34514103f8b5355f34c8
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-19 14:51:31 +0200

    add user entity structure

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/entities/User.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/entities/User.java
@@ -1,2 +1,22 @@
-package com.ukukhula.bursaryapi.entities;public class User {
+package com.ukukhula.bursaryapi.entities;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int contactId;
+    private int userRoleId;
+    private boolean isUserActive;
+
+    public User(int id, String firstName, String lastName, int contactId, int userRoleId, boolean isUserActive) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contactId = contactId;
+        this.userRoleId = userRoleId;
+        this.isUserActive = isUserActive;
+    }
 }
