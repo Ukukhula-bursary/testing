27f70b6bf0240e5c4a3295b2f147f0dd305ee3f7
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-20 10:30:57 +0200

    fix links

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/assemblers/StudentAllocationAssembler.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/assemblers/StudentAllocationAssembler.java
@@ -5,16 +5,27 @@ import com.ukukhula.bursaryapi.entities.StudentAllocation;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 
+import java.util.Map;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 public class StudentAllocationAssembler implements RepresentationModelAssembler<StudentAllocation, EntityModel<StudentAllocation>> {
 
+    private Map<String, Integer> parameters;
+
+    public void setParameters(Map<String, Integer> parameters) {
+        this.parameters = parameters;
+    }
+
     @Override
     public EntityModel<StudentAllocation> toModel(StudentAllocation studentAllocation) {
+        if (parameters == null) {
+            throw new IllegalArgumentException("Parameters must be set before calling toModel");
+        }
+
         return EntityModel.of(studentAllocation,
                 linkTo(methodOn(StudentAllocationController.class).getStudentAllocationById(studentAllocation.getId())).withSelfRel(),
                 linkTo(methodOn(StudentAllocationController.class).getAllStudentAllocations()).withRel("allStudentAllocations"),
-                // Add more links as needed
-                linkTo(methodOn(StudentAllocationController.class).getTotalSpentByUniversity(0, 0)).withRel("totalSpentByUniversity"));
+                linkTo(methodOn(StudentAllocationController.class).getTotalSpentByUniversity(parameters)).withRel("totalSpentByUniversity"));
     }
 }
