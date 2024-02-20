d09479b81d7066c7928f7a7c4ce8c5fe4fe4a7a3
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-20 04:11:30 +0200

    create interface

---
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/StudentAllocationRepository.java
@@ -0,0 +1,23 @@
+package com.ukukhula.bursaryapi.repositories;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import com.ukukhula.bursaryapi.entities.StudentAllocation;
+
+public interface StudentAllocationRepository {
+
+    List<StudentAllocation> getAllStudentAllocations();
+
+    StudentAllocation getStudentAllocationById(int id);
+
+    StudentAllocation createStudentAllocation(StudentAllocation studentAllocation);
+
+    StudentAllocation updateStudentAllocation(int id, StudentAllocation updatedAllocation);
+
+    void deleteStudentAllocation(int id);
+
+    BigDecimal getStudentAllocationsTotalSpent(int year);
+
+}
