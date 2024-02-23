d13f76ee1fa6adf8fc0e9782a844cce95de9ee84
mraees93 <raeesdolley76@gmail.com>
2024-02-23 14:48:53 +0200

    added addEventListener for login form

--- client/src/login.js
+++ client/src/login.js
@@ -2,4 +2,8 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const loginSubmitButton = document.getElementById("login-submit");
 
-loginSubmitButton.addEventListener("click");
+loginSubmitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  console.log("form submitted");
+});
