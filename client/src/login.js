01415b11cf6364ede5eb10512dfdf4483c2f38da
mraees93 <raeesdolley76@gmail.com>
2024-02-23 14:12:06 +0200

    added db connection string

--- client/src/login.js
+++ client/src/login.js
@@ -1,2 +1,5 @@
 const email = document.getElementById("email");
 const password = document.getElementById("password");
+const loginSubmitButton = document.getElementById("login-submit");
+
+loginSubmitButton.addEventListener("click");
