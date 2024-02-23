df2558ebe5d09130c4ac18e91db0ca6166e4c434
Manqoba-stack <manyoba1997@gmail.com>
2024-02-23 10:59:28 +0200

    inital commit

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/security/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package com.ukukhula.bursaryapi.security;
-
-public class SecurityConstants {
-    static final String SECRET_KEY = "ukubursmanagementapi";
-    static final long EXPIRATION_TIME = 3600000;
-}
